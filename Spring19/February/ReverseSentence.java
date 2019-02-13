30846b20b33d7c329468a78a00c3ae642e598930
Kevin Ramirez <kevinramirez266@yahoo.com>
2019-02-13 12:12:35 -0800

    Create ReverseSentence.java

---
+++ Spring19/February/ReverseSentence.java
@@ -0,0 +1,13 @@
+public class ReverseSentence {
+    public static void main(String[] args) {
+        String sentence = "Computer Science is hard";
+        /*
+            Suppose you are given a string which contains a sentence.
+            Create a method which returns nothing, has a parameter of a string, 
+                and prints out the sentence in reverse.
+        
+            Example: ReverseSent(sentence); //This should print out "hard is Science Computer";
+        */
+    }
+    //Insert your method below this line with a comment including your name.
+}
