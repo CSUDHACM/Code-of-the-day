6ec23559568c3fe7b47c7de669bf965a89c26d64
Kevin Ramirez <kevinramirez266@yahoo.com>
2019-03-13 14:35:29 -0700

    Create CheckHeap.java

---
+++ Spring19/March/CheckHeap.java
@@ -0,0 +1,15 @@
+
+public class CheckHeap {
+
+    public static void main(String[] args) {
+        int[] potentialHeap = {192, 173, 155, 130, 163, 124, 137, 60, 81, 2, 4, 113, 78, 77, 8};
+        /*
+      Heaps are a data structure found in comptuer science which allows easy organization of elements in ascending/descending order.
+      Max heaps follow the principle of having the parent greater than both children at every level.
+      For this Programming Practice, check if the given array is a max heap by using recursion.
+      Example: CheckMaxHeap(potentialHeap);   //Returns true if the array is a heap.
+         */
+    }
+
+    //Please write your submission below this line with your name in the comments.
+}
